ac3710e057621f706f10749d37fe653c242124d3
Roman Tsvyk <r.tsvyk@apiko.com>
2026-05-31 16:34:07 +0300

    First commit for OTP extension

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,143 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { useState, useEffect } from 'react';
+import speakeasy from '@levminer/speakeasy';
+
+import {
+  HELP_TEXT,
+  COPIED_MESSAGE,
+  STORAGE_TOKEN_KEY,
+  TOKEN_EXISTS_MESSAGE,
+  CODE_EXPIRATION_WARNING,
+  TOKEN_NOT_EXISTS_MESSAGE,
+} from './constants.js';
 import './Popup.css';
 
+function getChromeStorageToken(setToken) {
+  chrome.storage.local.get(STORAGE_TOKEN_KEY, function (result) {
+    setToken(result[STORAGE_TOKEN_KEY]);
+  });
+}
+
+function setChromeStorageToken(token) {
+  chrome.storage.local.set({ [STORAGE_TOKEN_KEY]: token });
+}
+
+export const debounce = (time) => (fn) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn(...args), time);
+  };
+};
+
+
 const Popup = () => {
+  const [code, setCode] = useState('');
+  const [value, setValue] = useState('');
+  const [error, setError] = useState('');
+  const [token, setToken] = useState(null);
+  const [copiedMessage, setCopiedMessage] = useState('');
+
+  const onInputChange = (e) => setValue(e.target.value);
+
+  const handleTokenChange = (newToken) => {
+    setToken(newToken);
+    setChromeStorageToken(newToken);
+    setError('');
+  };
+
+  const onTokenChange = (newToken) => {
+    if (!!value) {
+      handleTokenChange(newToken);
+      setValue('');
+    } else {
+      setError('Please provide valid 2FA token');
+    }
+  };
+
+  const clearCopiedMessage = debounce(5000)(
+    () => setCopiedMessage(CODE_EXPIRATION_WARNING),
+  );
+
+  const onGenerateCodeByToken = (actualToken) => {
+    const newCode = speakeasy.totp({ secret: actualToken, encoding: 'base32' });
+    setCode(newCode);
+    setError('');
+
+    navigator.clipboard.writeText(newCode);
+    setCopiedMessage(COPIED_MESSAGE);
+    clearCopiedMessage();
+  }
+
+  useEffect(() => {
+    getChromeStorageToken(setToken);
+  }, []);
+
+  const isCopied = copiedMessage === COPIED_MESSAGE;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+    <div className="popup">
+      <div className="text">
+        {token ? TOKEN_EXISTS_MESSAGE : TOKEN_NOT_EXISTS_MESSAGE}
+      </div>
+      <div className="row">
+        <input className="input" value={value} onChange={onInputChange} />
+        {!token && (
+          <button
+            className="btn insert"
+            onClick={() => onTokenChange(value)}
+          >
+            Insert
+          </button>
+        )}
+      </div>
+      {!!error && (
+        <div className="row">
+          <div className="error">
+            {error}
+          </div>
+        </div>
+      )}
+      {!!token && (
+        <>
+          <div className="row center">
+            <button
+              className="btn update"
+              onClick={() => onTokenChange(value)}
+            >
+              Update
+            </button>
+            <button
+              className="btn remove"
+              onClick={() => handleTokenChange(null)}
+            >
+              Remove
+            </button>
+          </div>
+          <div style={{ marginTop: 20 }} />
+          <div className="row center">
+            <div className="text">
+              {HELP_TEXT}
+            </div>
+          </div>
+          <div className="row center">
+            <input className="holder preview" value={code} readOnly />
+            <button
+              className="btn insert preview"
+              onClick={() => onGenerateCodeByToken(token)}
+            >
+              Generate
+            </button>
+          </div>
+          {!!copiedMessage && (
+            <div className="row center">
+              <div className={isCopied ? 'copied' : 'warning'}>
+                {copiedMessage}
+              </div>
+            </div>
+          )}
+          <div style={{ marginTop: 20 }} />
+        </>
+      )}
     </div>
   );
 };
---
+++ src/pages/Popup/constants.js
@@ -0,0 +1,11 @@
+export const STORAGE_TOKEN_KEY = '2faToken';
+
+export const TOKEN_EXISTS_MESSAGE = 'You already have cached 2FA token at Chrome Storage. You can generate code using it or update it to the new one';
+
+export const TOKEN_NOT_EXISTS_MESSAGE = '2FA token is missed. You need to insert it first to generate code';
+
+export const HELP_TEXT = 'Please click Generate button to receive your code';
+
+export const COPIED_MESSAGE = 'Code also was copied to your clipboard! ;)';
+
+export const CODE_EXPIRATION_WARNING = 'Your code can be expired after some time. Please re-generate it again to get the new one!'
